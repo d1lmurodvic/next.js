c5b28a936768367af63d855572b56d1d1380bfa1
d1lmurodvic <dildoraishmuhamedova4@gmail.com>
2025-08-15 04:00:54 +0500

    asd

--- src/components/drawers/DesktopSidebar.jsx
+++ src/components/drawers/DesktopSidebar.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import { RiDrinks2Fill } from "react-icons/ri";
+import { LuLayoutDashboard } from "react-icons/lu";
+
 
 const DesktopSidebar = () => {
     return (
-            <div className="drawer flex items-start justify-start overflow-y-auto h-screen">
-                <ul className=" bg-info-content/50 menu text-base-content min-h-full w-80 p-4">
-                    <li><a className='text-lg' href='/'>Home</a></li>
-                    <li><a className='text-lg' href='/drinks'>Choose</a></li>
-                </ul>
-            </div>
+        <div className="drawer flex items-start justify-start overflow-y-auto h-screen">
+            <ul className=" bg-info-content/50 menu text-base-content min-h-full w-80 p-4">
+                <li><a className='text-lg' href={`/`}><LuLayoutDashboard /> Home</a></li>
+                <li><a className='text-lg' href={`/drinks`}> <RiDrinks2Fill className=''/> Drinks Categroy</a></li>
+            </ul>
+        </div>
     )
 }
 
